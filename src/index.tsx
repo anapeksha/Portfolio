111528c7064763171b426da2d0aa76e25f6855af
anapeksha <anapeksha.mukherjee@gmail.com>
2022-09-12 09:42:36 +0530

    global styles added, animate.css added

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { GlobalStyles } from "@mui/material";
+import "animate.css";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+	document.getElementById("root") as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+	<React.StrictMode>
+		<GlobalStyles
+			styles={{
+				html: {
+					scrollBehavior: "smooth",
+				},
+				body: {
+					background: "hsl(213deg 72% 12%)",
+					color: "#cccccc",
+					WebkitFontSmoothing: "antialiased",
+					MozOsxFontSmoothing: "grayscale",
+				},
+			}}
+		/>
+		<App />
+	</React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
