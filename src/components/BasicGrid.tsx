c5e9114cd21565336f16deb79aac23c610f57f1f
anapeksha <anapeksha.mukherjee@gmail.com>
2022-09-24 00:38:56 +0530

    Grid component made

--- src/components/BasicGrid.tsx
+++ src/components/BasicGrid.tsx
@@ -1,14 +1,27 @@
-import { Box } from "@mui/material";
-import Grid from "@mui/material/Grid";
+import { Box, Grid } from "@mui/material";
+import IGridProps from "../interfaces/IGridProps";
+import Cards from "./Cards";
 
-const BasicGrid: React.FC = (props: any) => {
+const BasicGrid: React.FC<IGridProps> = (props) => {
 	return (
-		<Box sx={{ flexGrow: 1 }}>
+		<Box
+			style={{
+				display: "flex",
+				margin: "1.5vw",
+				flexGrow: 1,
+			}}
+		>
 			<Grid container spacing={2}>
-				{props.data.map((d: any, i: number) => {
+				{props.data.map((d, i) => {
 					return (
-						<Grid item xs={6}>
-							{d}
+						<Grid item xs={6} sm={4} md={3}>
+							<Cards
+								title={d.title}
+								description={d.description}
+								githubUrl={d.githubUrl}
+								techs={d.techs}
+								key={i}
+							/>
 						</Grid>
 					);
 				})}
