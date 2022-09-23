f748e88356050e6721a5fe59fb6ef797d2604cc2
anapeksha <anapeksha.mukherjee@gmail.com>
2022-09-24 00:39:44 +0530

    Made Popover for displaying resume

---
+++ src/components/Popover.tsx
@@ -0,0 +1,22 @@
+import CloseRoundedIcon from "@mui/icons-material/CloseRounded";
+import { Box, Dialog, DialogActions, IconButton, Zoom } from "@mui/material";
+
+const Popover = (props: any) => {
+	const handleClose = () => {
+		props.setOpen(false);
+	};
+	const pdf = require("../assets/resume/resume.pdf");
+	return (
+		<Dialog open={props.open} onClose={handleClose} TransitionComponent={Zoom}>
+			<Box style={{ backgroundColor: "hsl(213deg 72% 12%)" }}>
+				<DialogActions style={{ display: "flex", justifyContent: "start" }}>
+					<IconButton onClick={handleClose} style={{ color: "#cccccc" }}>
+						<CloseRoundedIcon fontSize="small" />
+					</IconButton>
+				</DialogActions>
+				<iframe src={pdf} title="resume" width="350px" height="500px"></iframe>
+			</Box>
+		</Dialog>
+	);
+};
+export default Popover;
