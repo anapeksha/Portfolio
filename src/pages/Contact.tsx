677a00acd2881597667fa4efe787d04c5681c299
anapeksha <anapeksha.mukherjee@gmail.com>
2022-09-24 00:40:15 +0530

    Contact page made

---
+++ src/pages/Contact.tsx
@@ -0,0 +1,12 @@
+import ContactBar from "../components/ContactBar";
+import {Box} from "@mui/material"
+
+const Contact = () => {
+	return (
+		<Box id="contact">
+			<ContactBar />
+		</Box>
+	);
+};
+
+export default Contact;
