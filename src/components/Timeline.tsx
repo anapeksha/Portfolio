1f42bd1815842d1bc6b3193f5a0f1338466fbdd3
anapeksha <anapeksha.mukherjee@gmail.com>
2022-09-29 16:59:19 +0530

    timeline component completed

--- src/components/Timeline.tsx
+++ src/components/Timeline.tsx
@@ -1,42 +1,36 @@
+import WorkIcon from "@mui/icons-material/Work";
 import Timeline from "@mui/lab/Timeline";
 import TimelineConnector from "@mui/lab/TimelineConnector";
 import TimelineContent from "@mui/lab/TimelineContent";
 import TimelineDot from "@mui/lab/TimelineDot";
 import TimelineItem from "@mui/lab/TimelineItem";
+import TimelineOppositeContent from "@mui/lab/TimelineOppositeContent";
 import TimelineSeparator from "@mui/lab/TimelineSeparator";
+import ITimelineProps from "../interfaces/ITimelineProps";
 
-function TimeLine() {
+const TimeLine: React.FC<ITimelineProps> = (props) => {
 	return (
 		<Timeline position="alternate">
-			<TimelineItem>
-				<TimelineSeparator>
-					<TimelineDot />
-					<TimelineConnector/>
-				</TimelineSeparator>
-				<TimelineContent>Eat</TimelineContent>
-			</TimelineItem>
-			<TimelineItem>
-				<TimelineSeparator>
-					<TimelineDot />
-					<TimelineConnector />
-				</TimelineSeparator>
-				<TimelineContent>Code</TimelineContent>
-			</TimelineItem>
-			<TimelineItem>
-				<TimelineSeparator>
-					<TimelineDot />
-					<TimelineConnector />
-				</TimelineSeparator>
-				<TimelineContent>Sleep</TimelineContent>
-			</TimelineItem>
-			<TimelineItem>
-				<TimelineSeparator>
-					<TimelineDot />
-				</TimelineSeparator>
-				<TimelineContent>Repeat</TimelineContent>
-			</TimelineItem>
+			{props.data.map((d, i) => {
+				return (
+					<TimelineItem key={i}>
+						<TimelineOppositeContent sx={{ m: "auto 0" }} variant="body1">
+							{d.company}
+						</TimelineOppositeContent>
+						<TimelineSeparator>
+							<TimelineDot color="secondary">
+								<WorkIcon color="primary" />
+							</TimelineDot>
+							<TimelineConnector sx={{ bgcolor: "secondary.main" }} />
+						</TimelineSeparator>
+						<TimelineContent sx={{ m: "auto 0" }} variant="body2">
+							{d.designation}
+						</TimelineContent>
+					</TimelineItem>
+				);
+			})}
 		</Timeline>
 	);
-}
+};
 
 export default TimeLine;
