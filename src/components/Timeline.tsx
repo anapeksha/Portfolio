b60fff2cb6b978c2d18f94ecbd934245642d7362
anapeksha <anapeksha.mukherjee@gmail.com>
2022-10-15 23:58:27 +0530

    animation modified

--- src/components/Timeline.tsx
+++ src/components/Timeline.tsx
@@ -20,8 +20,8 @@ const TimeLine: React.FC<ITimelineProps> = (props) => {
 						</TimelineOppositeContent>
 						<TimelineSeparator>
 							<motion.div
-								whileHover={{ scale: 1.2 }}
-								whileTap={{ scale: 1.2 }}
+								whileHover={{ scale: 0.9 }}
+								whileTap={{ scale: 0.9 }}
 								transition={{ type: "spring", stiffness: 400, damping: 10 }}
 							>
 								<TimelineDot color="secondary">
