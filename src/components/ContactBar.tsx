4c0c14f4980f16189dee64e4d6c5c8a746fbf5b2
anapeksha <anapeksha.mukherjee@gmail.com>
2022-10-16 09:59:00 +0530

    Contacts modified

--- src/components/ContactBar.tsx
+++ src/components/ContactBar.tsx
@@ -1,7 +1,5 @@
 import EmailIcon from "@mui/icons-material/Email";
-import FacebookIcon from "@mui/icons-material/Facebook";
 import GitHubIcon from "@mui/icons-material/GitHub";
-import InstagramIcon from "@mui/icons-material/Instagram";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
 import { Stack } from "@mui/material";
 import ContactButton from "./ContactButton";
@@ -9,12 +7,6 @@ import ContactButton from "./ContactButton";
 const ContactBar: React.FC = () => {
 	return (
 		<Stack direction="row" spacing={2} sx={{ mr: 4 }}>
-			<ContactButton url="https://instagram.com/naaz.xo">
-				<InstagramIcon fontSize="large" color="secondary" />
-			</ContactButton>
-			<ContactButton url="https://facebook.com/anapeksha.mukherjee">
-				<FacebookIcon fontSize="large" color="secondary" />
-			</ContactButton>
 			<ContactButton url="https://linkedin.com/in/anapeksha-mukherjee">
 				<LinkedInIcon fontSize="large" color="secondary" />
 			</ContactButton>
