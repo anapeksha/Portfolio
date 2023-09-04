c100933a9596489c0144a5ecd6529b516206c910
anapeksha <anapeksha.mukherjee@gmail.com>
2023-09-05 02:38:14 +0530

    Card for home

--- src/components/HomeCard.tsx
+++ src/components/HomeCard.tsx
@@ -1,9 +1,22 @@
-import { Card, CardContent } from "@mui/material";
+import { Card, CardContent, Stack, Typography } from "@mui/material";
 
-const HomeCard = () => {
+interface IHomeCard {
+	heading: string;
+	body: string;
+}
+
+const HomeCard = (props: IHomeCard) => {
 	return (
-		<Card sx={{ border: "2px solid #4b5486" }}>
-			<CardContent>Hello</CardContent>
+		<Card
+			sx={{ border: "1.5px solid #4b5486", backgroundColor: "#e4e8ff" }}
+			elevation={7}
+		>
+			<CardContent>
+				<Stack spacing={1}>
+					<Typography variant="h6">{props.heading}</Typography>
+					<Typography variant="body1">{props.body}</Typography>
+				</Stack>
+			</CardContent>
 		</Card>
 	);
 };
