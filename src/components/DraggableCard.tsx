b1c2afd5c1c4337e412bd0784ad58c441fec4efb
anapeksha <anapeksha.mukherjee@gmail.com>
2023-09-09 18:08:02 +0530

    motion Card

--- src/components/DraggableCard.tsx
+++ src/components/DraggableCard.tsx
@@ -1,5 +1,6 @@
-import { Card, CardActions, CardContent } from "@mui/material";
+import { Card, CardActions, CardContent, styled } from "@mui/material";
 import { ReactNode, CSSProperties } from "react";
+import { motion } from "framer-motion";
 
 interface IDraggableCard {
   style?: CSSProperties;
@@ -7,12 +8,25 @@ interface IDraggableCard {
   actions?: ReactNode;
 }
 
+const MotionCard = motion(Card, { forwardMotionProps: true });
+
+const CustomMotionCard = styled(MotionCard)({
+  ":hover": {
+    cursor: "move",
+  },
+});
+
 const DraggableCard = (props: IDraggableCard) => {
   return (
-    <Card style={props.style} elevation={6}>
+    <CustomMotionCard
+      style={props.style}
+      elevation={6}
+      drag
+      whileDrag={{ scale: 1.2 }}
+    >
       <CardContent>{props.children}</CardContent>
       <CardActions>{props.actions}</CardActions>
-    </Card>
+    </CustomMotionCard>
   );
 };
 
