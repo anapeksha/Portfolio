466933744247b07bb9c2329f56bddfa340c038e3
anapeksha <anapeksha.mukherjee@gmail.com>
2023-09-09 17:31:43 +0530

    draggable card implementation

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,6 +1,7 @@
-import { Box, Grid } from "@mui/material";
+import { Box, Chip, Grid, Stack, Typography } from "@mui/material";
+import { CheckCircle } from "@mui/icons-material";
 import AboutAvatar from "../assets/about-avatar.svg";
-import { aboutCardText } from "../lib/Constants";
+import { aboutCardText, skills, skillsHeading } from "../lib/Constants";
 import DraggableCard from "../components/DraggableCard";
 
 const About = () => {
@@ -12,16 +13,29 @@ const About = () => {
           height: "100vh",
           backgroundImage: `url(${AboutAvatar})`,
           backgroundSize: "100vw 100vh",
+          backgroundRepeat: "no-repeat",
           opacity: "0.9",
         }}
       >
-        <Grid container sx={{ p: 4 }} spacing={2}>
+        <Grid container spacing={2} sx={{ p: 4 }}>
           {aboutCardText.map((value, index) => {
             if (index % 2 === 0) {
               return (
                 <>
                   <Grid item xs={12} sm={6}>
-                    <DraggableCard body={value} key={index} />
+                    <DraggableCard
+                      key={index}
+                      style={{ backgroundColor: "#e1f8e8" }}
+                    >
+                      <Typography
+                        sx={{
+                          fontFamily: "'Fira Code', monospace",
+                          fontWeight: "700",
+                        }}
+                      >
+                        {value}
+                      </Typography>
+                    </DraggableCard>
                   </Grid>
                   <Grid item xs={12} sm={6}>
                     <Box></Box>
@@ -35,13 +49,49 @@ const About = () => {
                     <Box></Box>
                   </Grid>
                   <Grid item xs={12} sm={6}>
-                    <DraggableCard body={value} key={index} />
+                    <DraggableCard
+                      key={index}
+                      style={{ backgroundColor: "#e1f8e8" }}
+                    >
+                      <Typography
+                        sx={{
+                          fontFamily: "'Fira Code', monospace",
+                          fontWeight: "700",
+                        }}
+                      >
+                        {value}
+                      </Typography>
+                    </DraggableCard>
                   </Grid>
                 </>
               );
             }
           })}
         </Grid>
+      </Box>
+      <Box
+        sx={{
+          display: "flex",
+        }}
+      >
+        <Stack spacing={2} sx={{ ml: 4 }}>
+          <Typography gutterBottom>{skillsHeading}</Typography>
+          <Grid container rowGap={2}>
+            {skills.map((value, index) => {
+              return (
+                <Grid item xs={6} sm={4}>
+                  <Chip
+                    icon={<CheckCircle color="primary" />}
+                    label={value}
+                    key={index}
+                    color="secondary"
+                    sx={{ whiteSpace: "normal" }}
+                  />
+                </Grid>
+              );
+            })}
+          </Grid>
+        </Stack>
       </Box>
     </div>
   );
