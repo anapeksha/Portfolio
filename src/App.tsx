a67f0345e67f000fd8a90d8be24a2a314598361f
anapeksha <anapeksha.mukherjee@gmail.com>
2023-09-09 14:02:37 +0530

    new theme for contact page

--- src/App.tsx
+++ src/App.tsx
@@ -3,37 +3,37 @@ import { LocalizationProvider } from "@mui/x-date-pickers";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { Route, Routes, useLocation, Location } from "react-router-dom";
 import Header from "./components/Header";
-import { aboutTheme, homeTheme } from "./lib/Constants";
+import { aboutTheme, connectTheme, homeTheme } from "./lib/Constants";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
 import Home from "./pages/Home";
 
-const setTheme = (location: Location)=>{
-	if(location.pathname === "/home"){
-		return homeTheme;
-	}
-	else if(location.pathname === "/about"){
-		return aboutTheme;
-	}
-	else {
-		return homeTheme;
-	}
-}
+const setTheme = (location: Location) => {
+  if (location.pathname === "/home") {
+    return homeTheme;
+  } else if (location.pathname === "/about") {
+    return aboutTheme;
+  } else if (location.pathname === "/contact") {
+    return connectTheme;
+  } else {
+    return homeTheme;
+  }
+};
 
 function App() {
-	const location = useLocation();
-	return (
-		<LocalizationProvider dateAdapter={AdapterDayjs}>
-			<ThemeProvider theme={setTheme(location)}>
-				<Header />
-				<Routes>
-					<Route path="/" index element={<Home />} />
-					<Route path="contact" element={<Contact />} />
-					<Route path="about" element={<About />} />
-				</Routes>
-			</ThemeProvider>
-		</LocalizationProvider>
-	);
+  const location = useLocation();
+  return (
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
+      <ThemeProvider theme={setTheme(location)}>
+        <Header />
+        <Routes>
+          <Route path="/" index element={<Home />} />
+          <Route path="contact" element={<Contact />} />
+          <Route path="about" element={<About />} />
+        </Routes>
+      </ThemeProvider>
+    </LocalizationProvider>
+  );
 }
 
 export default App;
