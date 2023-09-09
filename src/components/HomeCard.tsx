8bc99154f922da2a544adb768f7cd82c24c33473
anapeksha <anapeksha.mukherjee@gmail.com>
2023-09-09 17:31:01 +0530

    background color changed

--- src/components/HomeCard.tsx
+++ src/components/HomeCard.tsx
@@ -1,24 +1,21 @@
 import { Card, CardContent, Stack, Typography } from "@mui/material";
 
 interface IHomeCard {
-	heading: string;
-	body: string;
+  heading: string;
+  body: string;
 }
 
 const HomeCard = (props: IHomeCard) => {
-	return (
-		<Card
-			sx={{ border: "1.5px solid #4b5486", backgroundColor: "#e4e8ff" }}
-			elevation={7}
-		>
-			<CardContent>
-				<Stack spacing={1}>
-					<Typography variant="h6">{props.heading}</Typography>
-					<Typography variant="body1">{props.body}</Typography>
-				</Stack>
-			</CardContent>
-		</Card>
-	);
+  return (
+    <Card sx={{ border: "1.5px solid #4b5486" }} elevation={7}>
+      <CardContent>
+        <Stack spacing={1}>
+          <Typography variant="h6">{props.heading}</Typography>
+          <Typography variant="body1">{props.body}</Typography>
+        </Stack>
+      </CardContent>
+    </Card>
+  );
 };
 
 export default HomeCard;
