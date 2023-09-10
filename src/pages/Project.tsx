00f824b8fdf2b5a14724c35d319eed74c48b044d
anapeksha <anapeksha.mukherjee@gmail.com>
2023-09-11 02:16:20 +0530

    Project page

---
+++ src/pages/Project.tsx
@@ -0,0 +1,29 @@
+import { Box, Stack } from "@mui/material";
+import { useState } from "react";
+import Laptop from "../components/Laptop";
+import Carousel from "../components/Carousel";
+import { carouselSteps } from "../lib/Constants";
+
+const Project = () => {
+  const [preview, setPreview] = useState<string>();
+  const [carouselStep, setCarouselStep] = useState(0);
+  return (
+    <div>
+      <Box
+        sx={{ display: "flex", justifyContent: "center", minHeight: "100vh" }}
+      >
+        <Stack spacing={-1}>
+          <Laptop src={preview as string} />
+          <Carousel
+            data={carouselSteps}
+            step={carouselStep}
+            setStep={setCarouselStep}
+            setPreview={setPreview}
+          />
+        </Stack>
+      </Box>
+    </div>
+  );
+};
+
+export default Project;
