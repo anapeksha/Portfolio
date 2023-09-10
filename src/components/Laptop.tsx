bee557bedde7c5c45c88b269bd92acb5a4f4af70
anapeksha <anapeksha.mukherjee@gmail.com>
2023-09-11 02:17:25 +0530

    component for showing webpage in laptop

---
+++ src/components/Laptop.tsx
@@ -0,0 +1,173 @@
+import { styled } from "@mui/material";
+
+interface ILaptop {
+  src: string;
+}
+
+const CustomSVG = styled("svg")({
+  ":hover": {
+    overscrollBehaviour: "none",
+  },
+});
+
+const Laptop = (props: ILaptop) => {
+  return (
+    <CustomSVG
+      version="1.1"
+      xmlns="http://www.w3.org/2000/svg"
+      xmlnsXlink="http://www.w3.org/1999/xlink"
+      x="0px"
+      y="0px"
+      viewBox="0 0 600 600"
+      enableBackground="new 0 0 600 600"
+      xmlSpace="preserve"
+    >
+      <g id="background">
+        <rect style={{ fill: "#FFFFFF" }} width="600" height="600" />
+      </g>
+      <g id="objects">
+        <g>
+          <linearGradient
+            id="SVGID_1_"
+            gradientUnits="userSpaceOnUse"
+            x1="300"
+            y1="443.5131"
+            x2="300"
+            y2="450.6771"
+          >
+            <stop offset="0.3564" style={{ stopColor: "#808185" }} />
+            <stop offset="0.5045" style={{ stopColor: "#78797D" }} />
+            <stop offset="0.5443" style={{ stopColor: "#7A7B7F" }} />
+            <stop offset="1" style={{ stopColor: "#0D0C11" }} />
+          </linearGradient>
+          <path
+            style={{ fill: `url(#SVGID_1_)` }}
+            d="M560,443.5c0,0-16,7-40,7s-416,0-440,0s-40-7-40-7l260-2L560,443.5z"
+          />
+
+          <linearGradient
+            id="SVGID_00000078035929020022557450000005707218627589330592_"
+            gradientUnits="userSpaceOnUse"
+            x1="300"
+            y1="149.5"
+            x2="300"
+            y2="439.701"
+          >
+            <stop offset="1.594388e-04" style={{ stopColor: "#363636" }} />
+            <stop offset="0.0092" style={{ stopColor: "#3A3A3A" }} />
+            <stop offset="0.0982" style={{ stopColor: "#404040" }} />
+            <stop offset="0.1199" style={{ stopColor: "#7D7D7D" }} />
+            <stop offset="0.1519" style={{ stopColor: "#404040" }} />
+            <stop offset="0.2384" style={{ stopColor: "#383838" }} />
+            <stop offset="0.4063" style={{ stopColor: "#404040" }} />
+            <stop offset="0.4336" style={{ stopColor: "#797979" }} />
+            <stop offset="0.4621" style={{ stopColor: "#404040" }} />
+            <stop offset="0.5727" style={{ stopColor: "#404040" }} />
+            <stop offset="0.5998" style={{ stopColor: "#626262" }} />
+            <stop offset="0.6294" style={{ stopColor: "#404040" }} />
+            <stop offset="0.6916" style={{ stopColor: "#404040" }} />
+            <stop offset="0.7905" style={{ stopColor: "#3D3D3D" }} />
+            <stop offset="0.9015" style={{ stopColor: "#404040" }} />
+            <stop offset="0.9334" style={{ stopColor: "#7E7E7E" }} />
+            <stop offset="0.9577" style={{ stopColor: "#474747" }} />
+          </linearGradient>
+          <path
+            style={{
+              fill: `url(#SVGID_00000078035929020022557450000005707218627589330592_)`,
+            }}
+            d="M511,433.5H89v-274
+			c0-5.523,4.477-10,10-10h402c5.523,0,10,4.477,10,10V433.5z"
+          />
+          <path d="M90.5,433.5v-274c0-4.687,3.813-8.5,8.5-8.5h402c4.687,0,8.5,3.813,8.5,8.5v274H90.5z" />
+
+          <linearGradient
+            id="SVGID_00000055694831397211487100000015672819596297080235_"
+            gradientUnits="userSpaceOnUse"
+            x1="40"
+            y1="438.5"
+            x2="560"
+            y2="438.5"
+          >
+            <stop offset="0" style={{ stopColor: "#49484D" }} />
+            <stop offset="0.0121" style={{ stopColor: "#D3D4D9" }} />
+            <stop offset="0.0282" style={{ stopColor: "#D0D1D6" }} />
+            <stop offset="0.0804" style={{ stopColor: "#8C8D92" }} />
+            <stop offset="0.0918" style={{ stopColor: "#85868B" }} />
+            <stop offset="0.5113" style={{ stopColor: "#CBCCD2" }} />
+            <stop offset="0.9264" style={{ stopColor: "#85868B" }} />
+            <stop offset="0.937" style={{ stopColor: "#8B8C91" }} />
+            <stop offset="0.979" style={{ stopColor: "#D0D1D6" }} />
+            <stop offset="0.994" style={{ stopColor: "#D3D4D9" }} />
+            <stop offset="1" style={{ stopColor: "#454449" }} />
+          </linearGradient>
+          <rect
+            x="40"
+            y="433.5"
+            style={{
+              fill: `url(#SVGID_00000055694831397211487100000015672819596297080235_)`,
+            }}
+            width="520"
+            height="10"
+          />
+
+          <linearGradient
+            id="SVGID_00000117648774767924435800000001890603929437513096_"
+            gradientUnits="userSpaceOnUse"
+            x1="258"
+            y1="437"
+            x2="342"
+            y2="437"
+          >
+            <stop offset="0" style={{ stopColor: "#161617" }} />
+            <stop offset="0.0581" style={{ stopColor: "#84848A" }} />
+            <stop offset="0.3011" style={{ stopColor: "#8F8F96" }} />
+            <stop offset="0.524" style={{ stopColor: "#ADADB5" }} />
+            <stop offset="0.7161" style={{ stopColor: "#8A8B91" }} />
+            <stop offset="0.9495" style={{ stopColor: "#7B7C81" }} />
+            <stop offset="1" style={{ stopColor: "#0D0C11" }} />
+          </linearGradient>
+          <path
+            style={{
+              fill: `url(#SVGID_00000117648774767924435800000001890603929437513096_)`,
+            }}
+            d="M335,440.5h-70c-3.866,0-7-3.134-7-7l0,0
+			h84l0,0C342,437.366,338.866,440.5,335,440.5z"
+          />
+          <line
+            style={{
+              fill: "none",
+              stroke: "#000000",
+              strokeWidth: 0.5,
+              strokeMiterlimit: 10,
+            }}
+            x1="509.5"
+            y1="424.5"
+            x2="90.5"
+            y2="424.5"
+          />
+
+          <foreignObject
+            width="398"
+            height="248"
+            x="101"
+            y="169.5"
+            style={
+              props.src
+                ? { backgroundColor: "white" }
+                : { backgroundColor: "#490B3D" }
+            }
+            display="flex"
+          >
+            <embed
+              src={props.src}
+              style={{ display: "flex", width: "100%", height: "100%" }}
+            />
+          </foreignObject>
+          <circle style={{ fill: "#232323" }} cx="300" cy="160.5" r="3" />
+        </g>
+      </g>
+    </CustomSVG>
+  );
+};
+
+export default Laptop;
