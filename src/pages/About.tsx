448fc2bb52bd5a13734315481bb4a144695f8014
anapeksha <anapeksha.mukherjee@gmail.com>
2023-09-13 14:23:26 +0530

    format change

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -14,90 +14,91 @@ import { aboutCardText, skills, skillsHeading } from "../lib/Constants";
 const About = () => {
   return (
     <div>
-      <Box
-        sx={{
-          display: "flex",
-          height: "100vh",
-          backgroundImage: `url(${AboutAvatar})`,
-          backgroundSize: "100vw 100vh",
-          backgroundRepeat: "no-repeat",
-          opacity: "0.9",
-        }}
-      >
-        <Grid container spacing={2} sx={{ p: 4 }}>
-          {aboutCardText.map((value, index) => {
-            if (index % 2 === 0) {
-              return (
-                <>
-                  <Grid item xs={12} sm={6} key={index}>
-                    <Card style={{ backgroundColor: "#e1f8e8" }}>
-                      <CardContent>
-                        <Typography
-                          sx={{
-                            fontFamily: "'Fira Code', monospace",
-                            fontWeight: "700",
-                          }}
-                        >
-                          {value}
-                        </Typography>
-                      </CardContent>
-                    </Card>
-                  </Grid>
-                  <Grid item xs={12} sm={6}>
-                    <Box></Box>
-                  </Grid>
-                </>
-              );
-            } else {
-              return (
-                <>
-                  <Grid item xs={12} sm={6}>
-                    <Box></Box>
-                  </Grid>
-                  <Grid item xs={12} sm={6}>
-                    <Card key={index} style={{ backgroundColor: "#e1f8e8" }}>
-                      <CardContent>
-                        <Typography
-                          sx={{
-                            fontFamily: "'Fira Code', monospace",
-                            fontWeight: "700",
-                          }}
-                        >
-                          {value}
-                        </Typography>
-                      </CardContent>
-                    </Card>
-                  </Grid>
-                </>
-              );
-            }
-          })}
-        </Grid>
-      </Box>
-      <Box
-        sx={{
-          display: "flex",
-        }}
-      >
-        <Stack spacing={2} sx={{ ml: 4 }}>
-          <Typography gutterBottom>{skillsHeading}</Typography>
-          <Grid container rowGap={2}>
-            {skills.map((value, index) => {
-              return (
-                <Grid item xs={6} sm={4}>
-                  <Chip
-                    icon={<CheckCircle color="primary" />}
-                    label={value}
-                    key={index}
-                    color="secondary"
-                    sx={{ whiteSpace: "normal" }}
-                  />
-                </Grid>
-              );
+      <Stack spacing={2}>
+        <Box
+          sx={{
+            display: "flex",
+            backgroundImage: `url(${AboutAvatar})`,
+            backgroundSize: "100vw 100vh",
+            backgroundRepeat: "no-repeat",
+            opacity: "0.9",
+          }}
+        >
+          <Grid container spacing={2} sx={{ p: 4 }}>
+            {aboutCardText.map((value, index) => {
+              if (index % 2 === 0) {
+                return (
+                  <>
+                    <Grid item xs={12} sm={6} key={index}>
+                      <Card style={{ backgroundColor: "#e1f8e8" }}>
+                        <CardContent>
+                          <Typography
+                            sx={{
+                              fontFamily: "'Fira Code', monospace",
+                              fontWeight: "700",
+                            }}
+                          >
+                            {value}
+                          </Typography>
+                        </CardContent>
+                      </Card>
+                    </Grid>
+                    <Grid item xs={12} sm={6}>
+                      <Box></Box>
+                    </Grid>
+                  </>
+                );
+              } else {
+                return (
+                  <>
+                    <Grid item xs={12} sm={6}>
+                      <Box></Box>
+                    </Grid>
+                    <Grid item xs={12} sm={6}>
+                      <Card key={index} style={{ backgroundColor: "#e1f8e8" }}>
+                        <CardContent>
+                          <Typography
+                            sx={{
+                              fontFamily: "'Fira Code', monospace",
+                              fontWeight: "700",
+                            }}
+                          >
+                            {value}
+                          </Typography>
+                        </CardContent>
+                      </Card>
+                    </Grid>
+                  </>
+                );
+              }
             })}
           </Grid>
-        </Stack>
-      </Box>
+        </Box>
+        <Box
+          sx={{
+            display: "flex",
+          }}
+        >
+          <Stack spacing={2} sx={{ ml: 4 }}>
+            <Typography gutterBottom>{skillsHeading}</Typography>
+            <Grid container rowGap={2}>
+              {skills.map((value, index) => {
+                return (
+                  <Grid item xs={6} sm={4}>
+                    <Chip
+                      icon={<CheckCircle color="primary" />}
+                      label={value}
+                      key={index}
+                      color="secondary"
+                      sx={{ whiteSpace: "normal" }}
+                    />
+                  </Grid>
+                );
+              })}
+            </Grid>
+          </Stack>
+        </Box>
+      </Stack>
     </div>
   );
 };
